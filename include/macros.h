21f51d62d17f208604c459d74ca610cbe9f349d0
RevoSucks <projectrevotpp@hotmail.com>
2024-02-17 23:47:12 -0500

    OSMutex

--- include/macros.h
+++ include/macros.h
@@ -11,11 +11,15 @@
 // This is dumb but we dont have a Metrowerks way to do variadic macros in the macro to make this done in a not scrubby way.
 #define ASSERTMSG1LINE(file, line, cond, msg, arg1) \
     ((cond) || (OSPanic(file, line, msg, arg1), 0))
+    
+#define ASSERTMSG2LINE(file, line, cond, msg, arg1, arg2) \
+    ((cond) || (OSPanic(file, line, msg, arg1, arg2), 0))
 
 #else
 #define ASSERTLINE(file, line, cond) (void)0
 #define ASSERTMSGLINE(file, line, cond, msg) (void)0
 #define ASSERTMSG1LINE(file, line, cond, msg, arg1) (void)0
+#define ASSERTMSG2LINE(file, line, cond, msg, arg1, arg2) (void)0
 #endif
     
 #define ASSERT(cond) ASSERTLINE(__FILE__, __LINE__, cond)
