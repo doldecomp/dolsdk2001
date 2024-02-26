3250d1665c62b4173fee45e41c5296305e6cb3e0
RevoSucks <projectrevotpp@hotmail.com>
2024-02-25 22:29:11 -0500

    CARDStat

--- include/dolphin/card.h
+++ include/dolphin/card.h
@@ -5,6 +5,13 @@
 #include <dolphin/dsp.h>
 #include <dolphin/dvd.h>
 
+#define CARD_FILENAME_MAX 32
+#define CARD_MAX_FILE 127
+#define CARD_ICON_MAX 8
+
+typedef void (*CARDCallback)(s32 chan, s32 result);
+
+#include <dolphin/card/CARDStat.h>
 #include <dolphin/card/CARDStatEx.h>
 #include <dolphin/card/CARDUnlock.h>
 #include <dolphin/card/CARDWrite.h>
@@ -27,8 +34,6 @@
 #define CARD_RESULT_CANCELED     -14
 #define CARD_RESULT_FATAL_ERROR -128
 
-typedef void (*CARDCallback)(s32 chan, s32 result);
-
 typedef struct CARDFileInfo
 {
     /*0x00*/ s32 chan;
@@ -37,29 +42,6 @@ typedef struct CARDFileInfo
     /*0x0C*/ s32 length;
     /*0x10*/ u16 iBlock;
 } CARDFileInfo;
-
-#define CARD_FILENAME_MAX 32
-#define CARD_MAX_FILE 127
-#define CARD_ICON_MAX 8
-
-typedef struct CARDStat
-{
-    /*0x00*/ char fileName[CARD_FILENAME_MAX];
-    /*0x20*/ u32 length;
-    /*0x24*/ u32 time;
-    /*0x28*/ u8 gameName[4];
-    /*0x2C*/ u8 company[2];
-    /*0x2E*/ u8 bannerFormat;
-    /*0x30*/ u32 iconAddr;
-    /*0x34*/ u16 iconFormat;
-    /*0x36*/ u16 iconSpeed;
-    /*0x38*/ u32 commentAddr;
-    /*0x3C*/ u32 offsetBanner;
-    /*0x40*/ u32 offsetBannerTlut;
-    /*0x44*/ u32 offsetIcon[CARD_ICON_MAX];
-    /*0x64*/ u32 offsetIconTlut;
-    /*0x68*/ u32 offsetData;
-} CARDStat;
 
 typedef struct CARDDir
 {
---
+++ src/card/CARDStat.c
@@ -0,0 +1,188 @@
+#include <dolphin.h>
+#include <dolphin/os.h>
+#include <dolphin/card.h>
+
+#define CARD_SYSTEM_BLOCK_SIZE (8 * 1024u)
+
+#define CARD_STAT_SPEED_END 0
+#define CARD_STAT_SPEED_FAST 1
+#define CARD_STAT_SPEED_MIDDLE 2
+#define CARD_STAT_SPEED_SLOW 3
+#define CARD_STAT_SPEED_MASK 3
+    
+// externally used internals
+extern struct CARDDir *__CARDGetDirBlock(struct CARDControl *card);
+extern void __CARDSyncCallback();
+
+#define CARD_READ_SIZE 512
+#define CARD_COMMENT_SIZE 64
+
+#define CARD_ICON_WIDTH 32
+#define CARD_ICON_HEIGHT 32
+
+#define CARD_BANNER_WIDTH 96
+#define CARD_BANNER_HEIGHT 32
+
+#define CARD_STAT_ICON_NONE 0
+#define CARD_STAT_ICON_C8 1
+#define CARD_STAT_ICON_RGB5A3 2
+#define CARD_STAT_ICON_MASK 3
+
+#define CARD_STAT_BANNER_NONE 0
+#define CARD_STAT_BANNER_C8 1
+#define CARD_STAT_BANNER_RGB5A3 2
+#define CARD_STAT_BANNER_MASK 3
+
+#define CARDGetBannerFormat(stat) (((stat)->bannerFormat) & CARD_STAT_BANNER_MASK)
+#define CARDGetIconFormat(stat, n) (((stat)->iconFormat >> (2 * (n))) & CARD_STAT_ICON_MASK)
+
+// functions
+static void UpdateIconOffsets(CARDDir *ent, CARDStat *stat);
+
+static void UpdateIconOffsets(CARDDir *ent, CARDStat *stat) {
+    u32 offset;
+    BOOL iconTlut;
+    int i;
+
+    offset = ent->iconAddr;
+    if (offset == 0xffffffff)
+    {
+        stat->bannerFormat = 0;
+        stat->iconFormat = 0;
+        stat->iconSpeed = 0;
+    }
+
+    iconTlut = FALSE;
+    switch (CARDGetBannerFormat(ent))
+    {
+    case CARD_STAT_BANNER_C8:
+        stat->offsetBanner = offset;
+        offset += CARD_BANNER_WIDTH * CARD_BANNER_HEIGHT;
+        stat->offsetBannerTlut = offset;
+        offset += 2 * 256;
+        break;
+    case CARD_STAT_BANNER_RGB5A3:
+        stat->offsetBanner = offset;
+        offset += 2 * CARD_BANNER_WIDTH * CARD_BANNER_HEIGHT;
+        stat->offsetBannerTlut = 0xffffffff;
+        break;
+    default:
+        stat->offsetBanner = 0xffffffff;
+        stat->offsetBannerTlut = 0xffffffff;
+        break;
+    }
+    for (i = 0; i < CARD_ICON_MAX; ++i)
+    {
+        switch (CARDGetIconFormat(ent, i))
+        {
+        case CARD_STAT_ICON_C8:
+            stat->offsetIcon[i] = offset;
+            offset += CARD_ICON_WIDTH * CARD_ICON_HEIGHT;
+            iconTlut = TRUE;
+            break;
+        case CARD_STAT_ICON_RGB5A3:
+            stat->offsetIcon[i] = offset;
+            offset += 2 * CARD_ICON_WIDTH * CARD_ICON_HEIGHT;
+            break;
+        default:
+            stat->offsetIcon[i] = 0xffffffff;
+            break;
+        }
+    }
+    if (iconTlut)
+    {
+        stat->offsetIconTlut = offset;
+        offset += 2 * 256;
+    }
+    else
+    {
+        stat->offsetIconTlut = 0xffffffff;
+    }
+    stat->offsetData = offset;
+}
+
+s32 CARDGetStatus(s32 chan, s32 fileNo, CARDStat *stat) {
+    CARDControl *card;
+    CARDDir *dir;
+    CARDDir *ent;
+    s32 result;
+
+    ASSERTLINE("CARDStat.c", 0x97, 0 <= chan && chan < 2);
+    ASSERTLINE("CARDStat.c", 0x98, 0 <= fileNo && fileNo < CARD_MAX_FILE);
+
+    if (fileNo < 0 || CARD_MAX_FILE <= fileNo)
+        return CARD_RESULT_FATAL_ERROR;
+
+    result = __CARDGetControlBlock(chan, &card);
+    if (result < 0)
+        return result;
+
+    dir = __CARDGetDirBlock(card);
+    ent = &dir[fileNo];
+    result = __CARDAccess(ent);
+    if (result == CARD_RESULT_NOPERM)
+        result = __CARDIsPublic(ent);
+
+    if (result >= 0)
+    {
+        memcpy(stat->gameName, ent->gameName, sizeof(stat->gameName));
+        memcpy(stat->company, ent->company, sizeof(stat->company));
+        stat->length = (u32)ent->length * card->sectorSize;
+        memcpy(stat->fileName, ent->fileName, CARD_FILENAME_MAX);
+        stat->time = ent->time;
+
+        stat->bannerFormat = ent->bannerFormat;
+        stat->iconAddr = ent->iconAddr;
+        stat->iconFormat = ent->iconFormat;
+        stat->iconSpeed = ent->iconSpeed;
+        stat->commentAddr = ent->commentAddr;
+
+        UpdateIconOffsets(ent, stat);
+    }
+    return __CARDPutControlBlock(card, result);
+}
+
+s32 CARDSetStatusAsync(s32 chan, s32 fileNo, CARDStat *stat, CARDCallback callback) {
+    CARDControl *card;
+    CARDDir *dir;
+    CARDDir *ent;
+    s32 result;
+
+    ASSERTLINE("CARDStat.c", 0xD5, 0 <= fileNo && fileNo < CARD_MAX_FILE);
+    ASSERTLINE("CARDStat.c", 0xD6, 0 <= chan && chan < 2);
+
+    if (fileNo < 0 || CARD_MAX_FILE <= fileNo)
+        return CARD_RESULT_FATAL_ERROR;
+
+    result = __CARDGetControlBlock(chan, &card);
+    if (result < 0)
+        return result;
+
+    dir = __CARDGetDirBlock(card);
+    ent = &dir[fileNo];
+    result = __CARDAccess(ent);
+    if (result < 0)
+        return __CARDPutControlBlock(card, result);
+
+    ent->bannerFormat = stat->bannerFormat;
+    ent->iconAddr = stat->iconAddr;
+    ent->iconFormat = stat->iconFormat;
+    ent->iconSpeed = stat->iconSpeed;
+    ent->commentAddr = stat->commentAddr;
+    UpdateIconOffsets(ent, stat);
+
+    ent->time = (u32)OSTicksToSeconds(OSGetTime());
+    result = __CARDUpdateDir(chan, callback);
+    if (result < 0)
+        __CARDPutControlBlock(card, result);
+    return result;
+}
+
+long CARDSetStatus(long chan, long fileNo, struct CARDStat * stat) {
+    long result = CARDSetStatusAsync(chan, fileNo, stat, __CARDSyncCallback);
+
+    if (result < 0) {
+        return result;
+    }
+    return __CARDSync(chan);
+}
