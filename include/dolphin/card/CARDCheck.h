03179b9ce172aa4ee7c18676c28dedce7adb0f54
camthehaxman <camthehaxman@users.noreply.github.com>
2024-03-15 15:46:05 -0500

    oops

--- include/dolphin/card/CARDCheck.h
+++ include/dolphin/card/CARDCheck.h
@@ -3,9 +3,8 @@
 
 #if DOLPHIN_REVISION >= 37
 s32 CARDCheckExAsync(s32 chan, s32 *xferBytes, CARDCallback callback);
-#else
-s32 CARDCheckAsync(s32 chan, CARDCallback callback);
 #endif
+s32 CARDCheckAsync(s32 chan, CARDCallback callback);
 long CARDCheck(long chan);
 
 #endif // _DOLPHIN_CARDCHECK_H_
