e8cbcc5c05a3faede14f98b3afbdc1c70a9df872
camthehaxman <camthehaxman@users.noreply.github.com>
2024-03-21 13:33:53 -0500

    HTable.c

--- include/charPipeline/structures.h
+++ include/charPipeline/structures.h
@@ -4,5 +4,6 @@
 #include <charPipeline/structures/List.h>
 #include <charPipeline/structures/dolphinString.h>
 #include <charPipeline/structures/Tree.h>
+#include <charPipeline/structures/HTable.h>
 
 #endif
