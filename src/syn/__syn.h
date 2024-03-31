2ebfb8fdb70ec8d8e939bb26eb45ba30621df3f2
RevoSucks <projectrevotpp@hotmail.com>
2024-03-31 19:04:28 -0400

    synpitch

--- src/syn/__syn.h
+++ src/syn/__syn.h
@@ -8,8 +8,13 @@ extern void __SYNRunVolumeEnvelope(struct SYNVOICE * voice);
 extern void __SYNRunLfo(struct SYNVOICE * voice);
 extern void __SYNRunPitchEnvelope(struct SYNVOICE * voice);
 extern void __SYNUpdateMix(struct SYNVOICE * voice);
-extern void __SYNUpdateSrc(struct SYNVOICE * voice);
 extern unsigned long __SYNGetNibbleAddress(unsigned long count);
+
+// synpitch.c
+float __SYNGetRelativePitch(struct SYNVOICE * voice);
+void __SYNSetupPitch(struct SYNVOICE * voice);
+void __SYNSetupSrc(struct SYNVOICE * voice);
+void __SYNUpdateSrc(struct SYNVOICE * voice);
 
 // synsample.c
 void __SYNSetupSample(struct SYNVOICE * voice);
