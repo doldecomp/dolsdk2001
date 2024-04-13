a511f7641d5d95a5e9422f5bd8fd9f2c4151dea9
RevoSucks <projectrevotpp@hotmail.com>
2024-04-12 22:46:24 -0400

    mcc, but the matches are very ugly.

--- include/dolphin/mcc.h
+++ include/dolphin/mcc.h
@@ -44,6 +44,7 @@ enum MCC_SYSEVENT {
 
 typedef void (* MCC_CBEvent)(enum MCC_CHANNEL channel, u32, u32);
 typedef void (* MCC_CBSysEvent)(enum MCC_SYSEVENT sysEvent);
+typedef int (* MCC_CBEnumDevices)(long);
 
 typedef struct {
     /* 0x00 */ u32 length;
@@ -51,13 +52,53 @@ typedef struct {
     /* 0x06 */ u16 protocol;
 } MCC_Hdr;
 
-// unsorted externs
-extern int MCCGetChannelInfo(enum MCC_CHANNEL chID, MCC_Info *info);
-extern int MCCInit(enum MCC_EXI exiChannel, u8 timeout, MCC_CBSysEvent callbackSysEvent);
-extern int MCCOpen(enum MCC_CHANNEL chID, u8 blockSize, MCC_CBEvent callbackEvent);
-extern u32 MCCSetChannelEventMask(enum MCC_CHANNEL chID, int unk);
-extern int MCCClose(enum MCC_CHANNEL chID);
-extern int MCCNotify(enum MCC_CHANNEL chID, int unk);
-extern int MCCWrite(enum MCC_CHANNEL chID, u32 offset, void *data, long size, int unk);
+typedef struct {
+    /* 0x00 */ MCC_Info info;
+    /* 0x04 */ MCC_CBEvent callbackEvent;
+    /* 0x08 */ u32 eventMask; // offset 0x8, size 0x4
+    /* 0x0C */ int isStreamDone; // offset 0xC, size 0x4
+    /* 0x10 */ int isStreamConnection; // offset 0x10, size 0x4
+} MCC_ChannelInfo;
+
+enum MCC_MODE {
+    MCC_MODE_MIN = 0,
+    MCC_MODE_MAX = 1,
+    MCC_MODE_ALL = 2,
+};
+
+enum MCC_CONNECT {
+    MCC_CONNECT_DISCONNECT = 0,
+    MCC_CONNECT_HOST_OPEN = 1,
+    MCC_CONNECT_TARGET_OPEN = 2,
+    MCC_CONNECT_CONNECTED = 3,
+};
+
+enum MCC_SYNC_STATE {
+    MCC_SYNC = 0,
+    MCC_ASYNC = 1,
+};
+
+// mcc.c
+int MCCStreamOpen(enum MCC_CHANNEL chID, u8 blockSize);
+int MCCStreamClose(enum MCC_CHANNEL chID);
+int MCCStreamWrite(enum MCC_CHANNEL chID, void *data, u32 dataBlockSize);
+u32 MCCStreamRead(enum MCC_CHANNEL chID, void *data);
+int MCCInit(enum MCC_EXI exiChannel, u8 timeout, MCC_CBSysEvent callbackSysEvent);
+void MCCExit(void);
+int MCCPing(void);
+int MCCEnumDevices(MCC_CBEnumDevices callbackEnumDevices);
+u8 MCCGetFreeBlocks(enum MCC_MODE mode);
+u8 MCCGetLastError(void);
+int MCCGetChannelInfo(enum MCC_CHANNEL chID, MCC_Info *info);
+int MCCGetConnectionStatus(enum MCC_CHANNEL chID, enum MCC_CONNECT *connect);
+int MCCNotify(enum MCC_CHANNEL chID, u32 notify);
+u32 MCCSetChannelEventMask(enum MCC_CHANNEL chID, u32 event);
+int MCCOpen(enum MCC_CHANNEL chID, u8 blockSize, MCC_CBEvent callbackEvent);
+int MCCClose(enum MCC_CHANNEL chID);
+int MCCLock(enum MCC_CHANNEL chID);
+int MCCUnlock(enum MCC_CHANNEL chID);
+int MCCRead(enum MCC_CHANNEL chID, u32 offset, void *data, long size, enum MCC_SYNC_STATE async);
+int MCCWrite(enum MCC_CHANNEL chID, u32 offset, void *data, long size, enum MCC_SYNC_STATE async);
+int MCCCheckAsyncDone();
 
 #endif // _DOLPHIN_MCC_H_
